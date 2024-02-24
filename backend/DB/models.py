ef2db4697c12ab01770280f5290088820ee51360
Aashish Venkatesh Gautham <aasvenk@iu.edu>
2024-02-23 20:58:13 -0500

    Adding User table

--- backend/DB/models.py
+++ backend/DB/models.py
@@ -2,25 +2,15 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from DB.database import Base
+from datetime import datetime,time
 
-
-class Books(Base):
-    __tablename__ = "books"
+class User(Base):
+    __tablename__ = "User"
 
     id = Column(Integer, primary_key=True,index= True)
-    title = Column(String)
-    Author = Column(String)
-    description = Column(String)
-    rating = Column(Integer)
-    
-
-# class Item(Base):
-#     __tablename__ = "items"
-
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String, index=True)
-#     description = Column(String, index=True)
-#     owner_id = Column(Integer, ForeignKey("users.id"))
-
-#     owner = relationship("User", back_populates="items")
-
+    firstName = Column(String)
+    lastName = Column(String)
+    emailId = Column(String)
+    dob= Column(datetime.date)
+    address = Column(String)
+    role = Column(String)
--- backend/DB/schema.py
+++ backend/DB/schema.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel,Field
-
-class Book(BaseModel): #Create Validations for the data that is being Commited to the DB. This is needed in FastAPI and is sometimes like the schema and metadata validations for the tables.
-    title :str = Field(min_length = 1)
-    author :str = Field(min_length = 1,max_length= 100)
-    description : str = Field(min_length = 1,max_length = 100)
-    rating : int = Field(gt = -1,lt=101)
+from datetime import datetime
+class User(BaseModel): 
+    firstName : str 
+    lastName : str
+    emailId :str
+    dob :datetime.date
+    address: str
+    role : str
+    
     
 
 
